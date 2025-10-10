7f5a8b0611214c97369f267f56b27dab20cd3ed9
https://github.com/GoncalvesFilipe <filipe.tec.informatica.if@gmail.com>
2025-10-10 16:02:54 -0300

    final updates

--- PacoteDowndoad/Curso Django/users/views.py
+++ PacoteDowndoad/Curso Django/users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from  django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def logout_view(request):
+    """Faz um logout no usuário."""
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    """Faz o cadatro de um novo usuário."""
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('index'))# Faz o redirecionamento forçado para index, caso o usuário não esteja logado, mesmo se ele tentar forçar com link.
+    
+    if request.method != 'POST':
+        # Exibe o formulário de cadastro em branco.
+        form = UserCreationForm()
+        
+    else: 
+        # Processa o formulário preenchido.
+        form = UserCreationForm(data=request.POST)
+        
+        if form.is_valid():
+            new_user = form.save()
+            # Faz login do usuário e o redireciona para a página inicial.
+            authenticated_user = authenticate(username = new_user.username, password = request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('index'))
+        
+    context = {'form': form}
+    return render(request,'users/register.html', context)
+        
